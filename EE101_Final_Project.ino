d78588e8eaf96f4a5a76cc516ab6eb131dc31cc7
EthanRR5 <123609084+EthanRR5@users.noreply.github.com>
2023-04-26 15:58:57 -0400

    Update EE101_Final_Project.ino

--- EE101_Final_Project.ino
+++ EE101_Final_Project.ino
@@ -1,11 +1,28 @@
+#include <SoftwareSerial.h> //BLE library
+SoftwareSerial mySerial(6,7);  //BLE TX is on pin2 and RX on pin 
+const int BuzzPin = 0; //buzzer pin
+const int VRx = 0; //analogpins for joystick
+const int VRy = 1; //^
+const int sw = 8;  //^^
+const int buttonPin = 12;  // the pin that the pushbutton is attached to
+const int inputPin = 13;               // choose the input pin (for PIR sensor)
+const int pirState = LOW;             // we start, assuming no motion detected
+const int solenoid = 10; //solenoid pin
+const int servopin=11;  //servopin
+// Variables will change:
+int servotimer = 50;        //time to move servo to new position
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;        // last state of the button
+int buzztimer = 1000;       //amount of time the buzzer will be on for in ms
+int val = 0;                    // variable for reading the motion sensor status
 //***************4x4*******************************************************************************************************
-int latchPin1 = 4;	// Latch pin of 74HC595 is connected to Digital pin 5
-int clockPin1 = 5;	// Clock pin of 74HC595 is connected to Digital pin 6
-int dataPin1 = 3;	// Data pin of 74HC595 is connected to Digital pin 4
+const int latchPin1 = 4;	// Latch pin of 74HC595 is connected to Digital pin 5
+const int clockPin1 = 5;	// Clock pin of 74HC595 is connected to Digital pin 6
+const int dataPin1 = 3;	// Data pin of 74HC595 is connected to Digital pin 4
 
-int latchPin2 = 1;	// Latch pin of 74HC595 is connected to Digital pin 1
-int clockPin2 = 2;	// Clock pin of 74HC595 is connected to Digital pin 2
-int dataPin2 = 0;	// Data pin of 74HC595 is connected to Digital pin 0
+const int latchPin2 = 1;	// Latch pin of 74HC595 is connected to Digital pin 1
+const int clockPin2 = 2;	// Clock pin of 74HC595 is connected to Digital pin 2
+const int dataPin2 = 0;	// Data pin of 74HC595 is connected to Digital pin 0
 
 byte ledcol = 0;		// Variable to hold the pattern of which LEDs are currently turned on or off
 byte ledrow = 0;
@@ -20,8 +37,14 @@ byte ledcol4 = 0b00000011;
  */
 void setup() 
 {
-  
-
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  //initialize motion sensor pin as input
+  pinMode(inputPin, INPUT);
+  //initialise solenoid pin as output
+  pinMode(solenoidpin, OUTPUT);
+  //initialize servo pin as output
+  pinMode(servopin, OUTPUT);
   // Set all the pins of 74HC595 as OUTPUT
   pinMode(latchPin1, OUTPUT);
   pinMode(dataPin1, OUTPUT);  
@@ -29,6 +52,68 @@ void setup()
   pinMode(latchPin2, OUTPUT);
   pinMode(dataPin2, OUTPUT);  
   pinMode(clockPin2, OUTPUT);
+  // initialize serial communication:
+  Serial.begin(9600);
+  
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    if (pirState == LOW) {
+      // we have just turned on
+      mySerial.begin(9600);   
+      delay(100);
+      mySerial.write("AT\r\n");  //put HM10 in AT command mode
+      delay(100);
+      mySerial.write("AT+NAME ERBLE\r\n");  //Name our HM10 something so as to not interfere with others
+      delay(100);
+      mySerial.write("AT+NAME\r\n");  //Verify new name       delay(100);
+      mySerial.write("AT+RESET\r\n");  //reset HM10 so new name will take effect
+      mySerial.println("Motion detected!");
+      mySerial.println("Would you like the wall at an angle? enter y for yes and n for no");
+      char angle = cGetCharFromPhone();
+      switch (input){
+        case 'y':{
+          //fire solenoid
+          digitalWrite(solenoid, HIGH);
+          //change servo angle
+          for (int i=0; i <= servotimer; i++){
+            digitalWrite(servopin,HIGH);
+            delay(2);
+            digitalWrite(servopin,LOW);
+            delay(18);  
+          }
+          break;
+        }
+        case 'n':{
+          //turn off solenoid
+          digitalWrite(solenoid, HIGH);
+          //reset servo angle
+          for (int i=0; i <= servotimer; i++){
+            digitalWrite(servopin,HIGH);
+            delay(1.5);
+            digitalWrite(servopin,LOW);
+            delay(18.5);  
+          break;
+        }
+        default{
+          mySerial.println("The input entered is not recognized, please reset the program and try again");
+          break;
+        }
+      }
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } 
+    else {
+      if (pirState == HIGH){
+        // we have just turned of
+        Serial.println("Motion ended!");
+        // We only want to print on the output change, not state
+        pirState = LOW;
+      }
+  }
+  
+  
+
 }
 
 
@@ -102,7 +187,28 @@ void loop()
   // updateShiftRegister();
   // ledcol = 0;
   // delay(1);
+  //***********Buzzer triggered by Button**********************************************************
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
 
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      for (int i; i <= buzztimer; i++){
+        digitalWrite(BuzzPin, HIGH);
+        delay(1);
+        digitalWrite(BuzzPin, LOW);
+      }
+    } 
+    else {
+      // if the current state is LOW then the button went from on to off: Turn off buzzer
+      digitalWrite(BuzzPin, LOW);
+    }
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
 }
 /*
  * updateShiftRegister() - This function sets the latchPin to low, then calls the Arduino function 'shiftOut' to shift out contents of variable 'leds' in the shift register before putting the 'latchPin' high again.
@@ -119,30 +225,6 @@ void updateShiftRegister()
    digitalWrite(latchPin1, HIGH);  
 }
 //*********BLE+Joystick+Buzzer****************************************************************************************************************************
-//modified from example code found on KY-023 DUAL AXIS JOYSTICK MODULE website - https://arduinomodules.info/ky-023-joystick-dual-axis-module/
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(6,7);  //TX is on pin2 and RX on pin 
-//buzzer pin
-const int BuzzPin = 0;
-//analogpins for joystick
-const int VRx = 0;
-const int VRy = 1;
-const int sw = 8;
-int direction; //initialize joystick input
-void setup(){
-  mySerial.begin(9600);   
-  Serial.begin(9600);   
-  delay(100);
-
-  mySerial.write("AT\r\n");  //put HM10 in AT command mode
-  delay(100);
-  mySerial.write("AT+NAME ERBLE\r\n");  //Name our HM10 something so as to not interfere with others
-  delay(100);
-  mySerial.write("AT+NAME\r\n");  //Verify new name
-  delay(100);
-  mySerial.write("AT+RESET\r\n");  //reset HM10 so new name will take effect
-
-}
 
 void loop(){
 
@@ -157,9 +239,7 @@ void loop(){
   switch (input){
     case '+':{
       //pwm to control buzzer tone
-      digitalWrite(BuzzPin, HIGH);
-      delay(1);
-      digitalWrite(BuzzPin, LOW);
+      
       break;
     }
     case '-':{
@@ -184,168 +264,7 @@ void loop(){
 //function cgetcharfromphone - inputs:none returns:char
 char cGetCharFromPhone(){
   int IInByte;                            //declare local variable
-  mySerial.println("Enter '+' to set the buzzer to high frequency, '-' to set it to low frequency, and '0' to turn it off");
-  mySerial.println("reset the program to change input");
   while (mySerial.available() == 0) {}     //wait for data available
   IInByte = mySerial.read();               //read character
   return(char(IInByte));
 }
-//**********************solenoid*************************************
-const int solenoid = 10;
-void setup() {
-
-
-void loop() {
-
-
-    Serial.println("Solenoid firing");
-    digitalWrite(solenoid, HIGH);
-    delay(200);
-    digitalWrite(solenoid, LOW);
-//********************servo*************************************
-//set digital I/O pin
-const int pulsepin=11;
-//set timer value
-int timer = 150;
-
-void setup() {
-
-  //initialize pulsepin as output
-  pinMode(pulsepin,OUTPUT);
-}
-
-void loop() {
-  //get angle from user via cgetcharfromkeyboard function
-  char angle=cGetCharFromKeyboard();
-//switch statement to choose angle
-  switch (angle){
-    case '+':{
-      //for loop to send pulse a number of times proportional to timer value
-      for (int i=0; i <= timer; i++){
-        digitalWrite(pulsepin,HIGH);
-        delay(1);
-        digitalWrite(pulsepin,LOW);
-        delay(19);
-      }
-      break;
-    }
-    case '-':{
-      for (int i=0; i <= timer; i++){
-        digitalWrite(pulsepin,HIGH);
-        delay(2);
-        digitalWrite(pulsepin,LOW);
-        delay(18);
-      }
-      break;
-    }
-    case '0':{
-      for (int i=0; i <= timer; i++){
-        digitalWrite(pulsepin,HIGH);
-        delay(1.5);
-        digitalWrite(pulsepin,LOW);
-        delay(18.5);
-      }
-      break;      
-    }
-    default:{
-      //input validation
-      Serial.println("The character you entered is not recognized, please try again");
-      delay(200);
-      break;
-    }
-  }
-}
-//*****************Push Button*************************************************************************
-/*
-  State change detection (edge detection)
-
-  Often, you don't need to know the state of a digital input all the time, but
-  you just need to know when the input changes from one state to another.
-  For example, you want to know when a button goes from OFF to ON. This is called
-  state change detection, or edge detection.
-
-  This example shows how to detect when a button or button changes from off to on
-  and on to off.
-
-  The circuit:
-  - pushbutton attached to pin 5 from +5V
-  - 10 kilohm resistor attached to pin 2 from ground
-
-  created  27 Sep 2005
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/StateChangeDetection
-*/
-
-// this constant won't change:
-const int buttonPin = 12;  // the pin that the pushbutton is attached to
-
-// Variables will change:
-int buttonState = 0;        // current state of the button
-int lastButtonState = 0;        // last state of the button
-
-void setup() {
-  // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
-  // initialize serial communication:
-  Serial.begin(9600);
-}
-
-
-void loop() {
-  // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
-
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
-    if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on:
-      Serial.println("on");
-    } 
-    else {
-      // if the current state is LOW then the button went from on to off:
-      Serial.println("off");
-    }
-  }
-  // save the current state as the last state, for next time through the loop
-  lastButtonState = buttonState;
-}
-//**********Motion Sensor***************************
-/*
- * PIR sensor tester - modified from Adafruit.com
- */
-int inputPin = 13;               // choose the input pin (for PIR sensor)
-int pirState = LOW;             // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
- 
-void setup() {
-  pinMode(inputPin, INPUT);     // declare sensor as input
- 
-  Serial.begin(9600);
-}
- 
-void loop(){
-  val = digitalRead(inputPin);  // read input value
-  if (val == HIGH) {            // check if the input is HIGH
-    if (pirState == LOW) {
-      // we have just turned on
-      Serial.println("Motion detected!");
-      // We only want to print on the output change, not state
-      pirState = HIGH;
-    }
-  } else {
-    if (pirState == HIGH){
-      // we have just turned of
-      Serial.println("Motion ended!");
-      // We only want to print on the output change, not state
-      pirState = LOW;
-    }
-  }
-  delay(500);
-}
-
-
