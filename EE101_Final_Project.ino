e5ee1ee020ffc76ccaea4adb86887d6b638bbdd5
EthanRR5 <123609084+EthanRR5@users.noreply.github.com>
2023-05-02 16:39:35 -0400

    Update EE101_Final_Project.ino

--- EE101_Final_Project.ino
+++ EE101_Final_Project.ino
@@ -12,10 +12,14 @@ const int servopin=11;  //servopin
 int servotimer = 50;        //time to move servo to new position
 int buttonState = 0;        // current state of the button
 int lastButtonState = 0;        // last state of the button
+int switchState = 0;           //current state of joystick switch
+int lastSwitchState = 0;      //last state of joystick switch
 int buzztimer = 1000;       //amount of time the buzzer will be on for in ms
 int val = 0;                    // variable for reading the motion sensor status
 int pirState = LOW;             // we start, assuming no motion detected
 int analogJoy = 0;           //analog joystick input
+bool joyflag = false;        //flag to open/close programming loop
+
 //***************4x4*******************************************************************************************************
 const int latchPin1 = 4;	// Latch pin of 74HC595 is connected to Digital pin 5
 const int clockPin1 = 5;	// Clock pin of 74HC595 is connected to Digital pin 6
@@ -27,10 +31,10 @@ const int dataPin2 = 0;	// Data pin of 74HC595 is connected to Digital pin 0
 
 byte ledcol = 0;		// Variable to hold the pattern of which LEDs are currently turned on or off
 byte ledrow = 0;
-byte ledcol1 = 0b00001010;
-byte ledcol2 = 0b00001001;
-byte ledcol3 = 0b00000100;
-byte ledcol4 = 0b00000011;
+byte ledcol1 = 0b00001000; //bytes that determine what leds in each column are turned on - only the last 4 digits are connected
+byte ledcol2 = 0b00000100;
+byte ledcol3 = 0b00000010;
+byte ledcol4 = 0b00000001;
 
 /*
  * setup() - this function runs once when you turn your Arduino on
@@ -53,8 +57,6 @@ void setup()
   pinMode(latchPin2, OUTPUT);
   pinMode(dataPin2, OUTPUT);  
   pinMode(clockPin2, OUTPUT);
-  // initialize serial communication:
-  Serial.begin(9600);
   
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
@@ -75,64 +77,33 @@ void setup()
       mySerial.println("Enter 'p' to program a new route, 'l' to load a route, or 'c' to keep the current route");
       char input = cGetCharFromPhone();
       switch (input){
-        case 'p':{
-          analogJoy = analogRead(VRx);	// read X axis value [0..1023]
-          mySerial.print("X:");
-          mySerial.println(analogJoy, DEC);
-          analogJoy = analogRead(VRy);	// read Y axis value [0..1023]
-          mySerial.print("Y:");
-          mySerial.println(analogJoy, DEC);
+        case 'p':
+            bool joyflag = true;
+            mySerial.println("Entering Programming mode")
+            mySerial.println("Use the joystick to navigate the board - press in to select a hold");
+            mySerial.println("Press in twice when you are done selecting holds");
           break;
-        }
-        case 'l':{
+        
+        case 'l':
           
           break;
-        }
-        case 'c':{
+        
+        case 'c':
           mySerial.println("Keeping the same route");
           break;
-        }
-        default:{
+        
+        default:
           mySerial.println("The input entered is not recognized, please reset the program and try again");
           break;
-        }
+        
       }
           
-      mySerial.println("Would you like the wall at an angle? enter 'y' for yes and 'n' for no");
-      char angle = cGetCharFromPhone();
-      switch (angle){
-        case 'y':{
-          //fire solenoid
-          digitalWrite(solenoid, HIGH);
-          //change servo angle
-          for (int i=0; i <= servotimer; i++){
-            digitalWrite(servopin,HIGH);
-            delay(2);
-            digitalWrite(servopin,LOW);
-            delay(18);  
-          }
-          break;
-        }
-        case 'n':{
-          //turn off solenoid
-          digitalWrite(solenoid, HIGH);
-          //reset servo angle
-          for (int i=0; i <= servotimer; i++){
-            digitalWrite(servopin,HIGH);
-            delay(1.5);
-            digitalWrite(servopin,LOW);
-            delay(18.5);  
-          break;
-        }
-        default:{
-          mySerial.println("The input entered is not recognized, please reset the program and try again");
-          break;
-        }
-      }
+      
       // We only want to print on the output change, not state
       pirState = HIGH;
     }
     } 
+    }
       else {
         if (pirState == HIGH){
           // we have just turned of
@@ -146,17 +117,44 @@ void setup()
 
 
 
+
 /*
  * loop() - this function runs over and over again
  */
 void loop() 
 {
+  if (joyflag == true){
+    analogJoy = analogRead(VRx);	// read X axis value [0..1023]
+    mySerial.print("X:");
+    mySerial.println(analogJoy, DEC);
+    analogJoy = analogRead(VRy);	// read Y axis value [0..1023]
+    mySerial.print("Y:");
+    mySerial.println(analogJoy, DEC);
+    delay(100);
+    switchState = digitalRead(sw);
+    
+  //   // compare the switchState to its previous state
+  //   if (switchState != lastSwitchState) {
+  //     // if the state has changed, increment the counter
+  //     if (switchState == HIGH) {
+  //       // if the current state is HIGH then the button went from off to on:
+  //       } 
+  //     else {
+  //       // if the current state is LOW then the button went from on to off: Turn off buzzer
+  //       digitalWrite(BuzzPin, LOW);
+  //     }
+  //   }
+  //   // save the current state as the last state, for next time through the loop
+  //   lastButtonState = buttonState; 
+  // }
+
   ledcol = 0;
   ledrow = 0;	// Initially turns all the LEDs off, by giving the variable 'leds' the value 0
 
   updateShiftRegister();
     for (int i = 0; i < 4; i++)	// Turn all the LEDs ON one by one.
   {
+    ledcol = 0;
     bitSet(ledrow, i);		// Set the bit that controls that LED in the variable 'leds'
     updateShiftRegister();
     if(i==0) {
@@ -217,27 +215,28 @@ void loop()
   // delay(1);
   //***********Buzzer triggered by Button**********************************************************
   // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
+//   buttonState = digitalRead(buttonPin);
 
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
-    if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on:
-      for (int i; i <= buzztimer; i++){
-        digitalWrite(BuzzPin, HIGH);
-        delay(1);
-        digitalWrite(BuzzPin, LOW);
-      }
-    } 
-    else {
-      // if the current state is LOW then the button went from on to off: Turn off buzzer
-      digitalWrite(BuzzPin, LOW);
-    }
-  }
-  // save the current state as the last state, for next time through the loop
-  lastButtonState = buttonState;
-}
+//   // compare the buttonState to its previous state
+//   if (buttonState != lastButtonState) {
+//     // if the state has changed, increment the counter
+//     if (buttonState == HIGH) {
+//       mySerial.println("Congratulations on finishing the climb!");
+//       // if the current state is HIGH then the button went from off to on:
+//       for (int i; i <= buzztimer; i++){
+//         digitalWrite(BuzzPin, HIGH);
+//         delay(1);
+//         digitalWrite(BuzzPin, LOW);
+//       }
+//     } 
+//     else {
+//       // if the current state is LOW then the button went from on to off: Turn off buzzer
+//       digitalWrite(BuzzPin, LOW);
+//     }
+//   }
+//   // save the current state as the last state, for next time through the loop
+//   lastButtonState = buttonState;
+   }
 /*
  * updateShiftRegister() - This function sets the latchPin to low, then calls the Arduino function 'shiftOut' to shift out contents of variable 'leds' in the shift register before putting the 'latchPin' high again.
  */
@@ -258,4 +257,38 @@ char cGetCharFromPhone(){
   while (mySerial.available() == 0) {}     //wait for data available
   IInByte = mySerial.read();               //read character
   return(char(IInByte));
+}
+//function wallangle - inputs:none returns:none
+void wallAngle(){
+  mySerial.println("Would you like the wall at an angle? enter 'y' for yes and 'n' for no");
+  char angle = cGetCharFromPhone();
+  switch (angle){
+    case 'y':
+      //fire solenoid
+      digitalWrite(solenoid, HIGH);
+      //change servo angle
+      for (int i=0; i <= servotimer; i++){
+        digitalWrite(servopin,HIGH);
+        delay(2);
+        digitalWrite(servopin,LOW);
+        delay(18);  
+      }
+      break;
+    
+    case 'n':
+      //turn off solenoid
+      digitalWrite(solenoid, HIGH);
+      //reset servo angle
+      for (int i=0; i <= servotimer; i++){
+        digitalWrite(servopin,HIGH);
+        delay(1.5);
+        digitalWrite(servopin,LOW);
+        delay(18.5);  
+      break;
+    
+    default:
+      mySerial.println("The input entered is not recognized, please reset the program and try again");
+      break;
+    
+  }
 }
